93a4429f788c2faa44d27bedfe42482212e9a93b
reloadercf <elcfm1@gmail.com>
2020-11-17 18:42:41 -0600

    add socket emit and on

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ const io = require('socket.io')(server);
 //desplegar el directorio publico
 app.use(express.static(__dirname+'/public'))
 
-io.on('connection', () => {
-    console.log('Cliente conectado')
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.emit('mensaje-bienvenida', 'Bienvenido al server')
+    socket.on('mensaje-cliente',(data)=>{
+        console.log(data)
+    })
 });
 
 server.listen(8080,()=>console.log('Server en puerto 8080'));
